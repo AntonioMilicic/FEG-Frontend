7e645166031859e7792fa1a3c1212392189926a0
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-12 15:17:37 +0200

    Initial commit with project set up

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  pages: {
+    index: {
+      entry: path.join(__dirname, 'client/main.js'),
+      favicon: path.join(__dirname, 'client/assets/favicon.ico')
+    }
+  },
+  configureWebpack: ({
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'client')
+      }
+    }
+  })
+};
