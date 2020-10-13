68a4d8b47f2213cb070d0e0fae853bcac5e2f080
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-13 13:00:44 +0200

    Add shared base link component

---
+++ client/components/shared/BaseLink.vue
@@ -0,0 +1,49 @@
+<template>
+  <a :href="href" :class="{ [color]: true }">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+const behaviour = ['image', 'white', 'gray-light-100'];
+
+export default {
+  name: 'base-link',
+  props: {
+    href: {
+      type: String,
+      default: '#'
+    },
+    color: {
+      type: String,
+      validator: val => behaviour.includes(val),
+      default: behaviour[0]
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.image {
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+
+.white {
+  color: var(--color-white);
+
+  &:hover {
+    color: var(--color-gray-light-100);
+    border-bottom: 2px solid var(--color-gray-light-100);
+  }
+}
+
+.gray-light-100 {
+  color: var(--color-gray-light-100);
+
+  &:hover {
+    color: var(--color-white);
+  }
+}
+</style>
