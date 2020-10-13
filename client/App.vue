95dc7a634d9f57f455ae732d0a43b4007ee79179
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-13 12:43:49 +0200

    Add header view and footer to project, import scss assets

--- client/App.vue
+++ client/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
+  <div class="app">
+    <header-component />
     <router-view />
+    <footer-component />
   </div>
 </template>
 
 <script>
+import FooterComponent from '@/components/Footer';
+import HeaderComponent from '@/components/Header';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { FooterComponent, HeaderComponent }
 };
 </script>
+
+<style lang="scss">
+@import "~@/assets/stylesheets/main";
+
+.app {
+  height: 100%;
+}
+</style>
