d06a460d97c1a315dc95377388dd5875d27d6e08
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-13 12:59:31 +0200

    Add header file component

---
+++ client/components/Header/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="header-container flex-h justify-space-between px-m">
+    <link-list :links="links" class="header-left" />
+    <user-content :user-name="userName" :password="password" class="header-right" />
+  </nav>
+</template>
+
+<script>
+import LinkList from './LinkList';
+import UserContent from '@/components/Header/UserContent';
+
+const links = ['Sport', 'Uživo', 'Loto', 'Casino', 'Vegas',
+  'Igre Uživo', 'Virtualne Igre'];
+
+export default {
+  name: 'header-component',
+  data: () => ({
+    links,
+    userName: 'Antonio',
+    password: '******'
+  }),
+  components: { LinkList, UserContent }
+};
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  height: var(--header-height);
+  background-color: var(--color-primary-300);
+}
+</style>
