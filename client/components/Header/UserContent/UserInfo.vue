ccd51fefa1b1f3a4866a34a5a1d95c534b5379be
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-13 13:02:32 +0200

    Add user info container

---
+++ client/components/Header/UserContent/UserInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="user-info flex-h justify-space-between pb-xxs p1">
+    <div class="ml-xxxs">{{ userName }}</div>
+    <div>
+      {{ password }}
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-info',
+  props: {
+    userName: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.user-info {
+  border-bottom: 3px solid var(--color-black-200);
+}
+</style>
