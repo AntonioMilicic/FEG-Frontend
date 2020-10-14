bb84979216f48f174e408fba402b1a45ce16b5ff
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-14 20:00:02 +0200

    Add routes for new navbar

--- client/router.js
+++ client/router.js
@@ -6,8 +6,43 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Main',
-    component: () => import('@/components/Main')
+    name: 'main',
+    component: () => import('@/components/Main'),
+    children: [
+      { path: 'popular', name: 'popular' },
+      { path: 'new', name: 'new' },
+      { path: 'slots', name: 'slots' },
+      { path: 'table', name: 'table-games' },
+      { path: 'jackpot', name: 'jackpot' },
+      {
+        path: '/tournament',
+        component: () => import('@/components/Main/Tournament'),
+        children: [
+          {
+            path: '',
+            name: 'tournaments',
+            redirect: { name: 'active' }
+          },
+          {
+            path: 'active',
+            name: 'active',
+            component: () => import('@/components/Main/Tournament/Active')
+          },
+          {
+            path: 'upcoming',
+            name: 'upcoming',
+            component: () => import('@/components/Main/Tournament/Upcoming')
+          },
+          {
+            path: 'finished',
+            name: 'finished',
+            component: () => import('@/components/Main/Tournament/Finished')
+          }
+        ]
+      },
+      { path: 'promotion', name: 'promotions' },
+      { path: 'games', name: 'my-games' }
+    ]
   }
 ];
 
