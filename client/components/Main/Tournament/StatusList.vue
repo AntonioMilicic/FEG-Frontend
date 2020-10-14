a1724d5128fb5feeb81aa414a9194d1fd8132443
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-14 20:11:45 +0200

    Add status bar for active, upcoming and finished tournaments

---
+++ client/components/Main/Tournament/StatusList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="flex-h mx-xl px-xxs">
+    <base-router-link
+      v-for="(tournamentNum, name) in status"
+      :key="name"
+      :name="name"
+      class="link px-l mx-xs py-xxs h1">
+      {{ name | capital }} ({{ tournamentNum }})
+    </base-router-link>
+  </ul>
+</template>
+
+<script>
+import BaseRouterLink from '@/components/shared/BaseRouterLink';
+
+export default {
+  name: 'status-list',
+  props: {
+    status: {
+      type: Object,
+      required: true
+    }
+  },
+  filters: {
+    capital: value => (value.charAt(0).toUpperCase() + value.slice(1))
+  },
+  components: { BaseRouterLink }
+};
+</script>
