21c946497a720237f2da9ca741ee80e2aa74761c
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-14 19:57:05 +0200

    Add navbar for casino view

---
+++ client/components/Main/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <base-navbar>
+    <template #left>
+      <font-awesome-icon icon="home" size="lg" class="nav-icon mr-s" />
+      <base-router-link
+        v-for="{ category, name } in tabs"
+        :key="name"
+        :name="name"
+        class="tab-item px-xs mx-xxxs h3">
+        {{ category }}
+      </base-router-link>
+    </template>
+    <template #right>
+      <div class="search flex-h align-center">
+        <font-awesome-icon icon="search" class="nav-icon mx-xs" />
+        <input type="text" placeholder="Pretraži preko 300 igara..." class="input mx-xxs">
+      </div>
+    </template>
+  </base-navbar>
+</template>
+
+<script>
+import BaseNavbar from '@/components/shared/BaseNavbar';
+import BaseRouterLink from '@/components/shared/BaseRouterLink';
+
+const tabs = [
+  { category: 'Popularno', name: 'popular' },
+  { category: 'Novo', name: 'new' },
+  { category: 'Automati', name: 'slots' },
+  { category: 'Igre na stolovima', name: 'table-games' },
+  { category: 'Jackpot', name: 'jackpot' },
+  { category: 'Turniri', name: 'tournaments' },
+  { category: 'Promocije', name: 'promotions' },
+  { category: 'Moje igre', name: 'my-games' }
+];
+
+export default {
+  name: 'main-navbar',
+  data: () => ({
+    tabs
+  }),
+  components: { BaseNavbar, BaseRouterLink }
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-icon {
+  color: var(--color-white);
+}
+
+.tab-item {
+  padding-top: 7px;
+  padding-bottom: 7px;
+}
+
+.router-link-active {
+  padding-top: 10px;
+}
+
+.search {
+  width: 15rem;
+  height: 2rem;
+  background-color: var(--color-gray-dark-300);
+  border-radius: var(--border-radius-xxxs);
+
+  .input {
+    width: 80%;
+    color: var(--color-white);
+    background-color: inherit;
+    border: none;
+  }
+}
+</style>
