7f1ae99c2d0652e48259c04d74cea9ef811df0f3
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-14 20:00:38 +0200

    Add base button component

---
+++ client/components/shared/BaseButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    v-on="$listeners"
+    v-bind="$attrs"
+    :class="{ [color]: true }"
+    class="button">
+    <slot>Click me</slot>
+  </button>
+</template>
+
+<script>
+
+const colors = ['secondary', 'gray'];
+
+export default {
+  name: 'base-button',
+  props: {
+    color: {
+      type: String,
+      validator: val => colors.includes(val),
+      default: colors[0]
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  color: white;
+  border: none;
+  border-radius: var(--border-radius-xxxs);
+  cursor: pointer;
+
+  &.secondary {
+    background-color: var(--color-secondary);
+  }
+
+  &.gray {
+    background-color: var(--color-gray-light-200);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
