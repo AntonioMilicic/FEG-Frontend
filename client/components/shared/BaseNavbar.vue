d02b9cf3ab9aac67abe4cf9cd72e6ab5275f00bd
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-14 19:57:58 +0200

    Add base navbar component, need to use it for header in next pr

---
+++ client/components/shared/BaseNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="base-navbar flex-h align-center justify-space-between px-m">
+    <div class="side-left flex-h align-center">
+      <slot name="left"></slot>
+    </div>
+    <div class="side-right flex-h align-center">
+      <slot name="right"></slot>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'base-navbar'
+};
+</script>
+
+<style lang="scss" scoped>
+.base-navbar {
+  height: var(--nav-height);
+  background-color: var(--color-black);
+}
+</style>
