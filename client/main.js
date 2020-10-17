a5621870fc76764fc3fa17b7a2244233e922d714
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-17 15:07:52 +0200

    Add new icons and vue material dialog to project

--- client/main.js
+++ client/main.js
@@ -1,19 +1,21 @@
 import {
-  faCaretDown, faCheck, faEye, faHome,
-  faSearch, faTrophy, faWallet
+  faCaretDown, faCheck, faChevronLeft, faChevronRight,
+  faEye, faHome, faSearch, faTrophy, faWallet
 } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { MdDialog } from 'vue-material/dist/components';
 import router from './router';
 import store from './store/store';
 import Vue from 'vue';
 
-library.add(faCaretDown, faCheck, faEye, faFacebookF,
-  faHome, faSearch, faWallet, faTrophy);
-
+library.add(faCaretDown, faCheck, faChevronLeft, faChevronRight,
+  faEye, faFacebookF, faHome, faSearch, faWallet, faTrophy);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(MdDialog);
 
 Vue.config.productionTip = false;
 
