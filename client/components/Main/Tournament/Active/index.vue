7f0bcd196f5d59547eb705c0bbd31304e2e25df5
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-18 15:53:36 +0200

    Add modal for active, calculations are done because of random number

--- client/components/Main/Tournament/Active/index.vue
+++ client/components/Main/Tournament/Active/index.vue
@@ -27,16 +27,50 @@
 </template>
 
 <script>
+import { getGameCarousel, getRanking } from '@/helpers/api/UserGames';
 import GameForm from '../tournament-form/GameForm';
 import { mapGetters } from 'vuex';
+import RankingModal from '../ranking/RankingForm';
 import TournamentMessage from '../message/TournamentErrorMessage';
+const gameStats = [
+  { title: 'Players', content: '' },
+  { title: 'Registered', content: '487' }, // The f? This data shouldnt be part of active
+  { title: 'Registration', content: 'Finished' }, // Nor this
+  { title: 'Included games', content: '58' }
+];
 
 export default {
   name: 'active-tournament',
   computed: { ...mapGetters({ games: 'activeGameData' }) },
   methods: {
-    showDetails() {
-      console.log('Show modal');
+    showDetails({ image, gameStateDescription, gameRules }) {
+      const game = {
+        title: gameStateDescription.title,
+        rules: gameRules,
+        image
+      };
+      const getRanks = getRanking;
+      getRanks[getRanks.length - 1].rank = gameStateDescription.totalPlayers;
+      const ranking = getRanks;
+      const gameStatsCalc = gameStats;
+      gameStatsCalc[0].content = gameStateDescription.totalPlayers;
+      const gameStatistic = gameStatsCalc;
+      const gameCarousel = getGameCarousel;
+
+      this.$modal.show(
+        RankingModal,
+        {
+          game,
+          ranking,
+          gameStats: gameStatistic,
+          gameCarousel
+        },
+        {
+          height: 'auto',
+          shiftY: 0,
+          scrollable: true
+        }
+      );
     }
   },
   components: { GameForm, TournamentMessage }
