d126a8fcfc92814d0ac6dd870e398a856c741bf7
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-18 15:20:45 +0200

    Use vmodal to position modal properly in DOM

--- client/main.js
+++ client/main.js
@@ -6,16 +6,16 @@ import App from './App.vue';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { MdDialog } from 'vue-material/dist/components';
 import router from './router';
 import store from './store/store';
+import VModal from 'vue-js-modal';
 import Vue from 'vue';
 
 library.add(faCaretDown, faCheck, faChevronLeft, faChevronRight,
   faEye, faFacebookF, faHome, faSearch, faWallet, faTrophy);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-Vue.use(MdDialog);
+Vue.use(VModal);
 
 Vue.config.productionTip = false;
 
