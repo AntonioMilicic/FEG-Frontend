64ed73cdbb412a9c35f4bb6d193ab4f7e7fac6c7
Antonio Milicic <Antonio.Milicic.00@fesb.hr>
2020-10-18 15:45:25 +0200

    Import and use components for ranking form

--- client/components/Main/Tournament/ranking/RankingForm.vue
+++ client/components/Main/Tournament/ranking/RankingForm.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="ranking-form pb-l">
-    <ranking-header :image="game.image" :game-state="currentRouteName" />
+    <ranking-header
+      :image="game.image"
+      :title="game.title"
+      :is-active="isActive" />
     <div class="ranking-wrapper mx-s">
       <ranking-table
-        :game-state="currentRouteName"
         :ranking="ranking"
         :user-rank="userRank" />
       <game-description
         :rules="game.rules"
-        :game-stats="gameStateDescription" />
-      <game-carousel :images="gameCarousel.images" :total="gameCarousel.total" />
+        :game-stats="gameStats" />
+      <game-carousel
+        :images="gameCarousel.images"
+        :total="gameCarousel.total" />
       <base-button
         behaviour="inherit"
         type="button"
@@ -22,38 +26,18 @@
 
 <script>
 import BaseButton from '@/components/shared/BaseButton';
-import GameCarousel from '@/components/shared/ranking/GameCarousel';
-import GameDescription from '@/components/shared/ranking/GameDescription';
-import RankingHeader from '@/components/shared/ranking/RankingHeader';
-import RankingTable from '@/components/shared/ranking/RankingTable';
-// const players = [
-//   { rank: 1, username: 'Tonitest1', score: 1000, prize: 1000 },
-// ];
-// const game = {
-//   title,
-//   image,
-//   rules
-// };
-// const gameStateDescription = [
-//   { title: 'Players', content: '1521' },
-//   { title: 'Registered', content: '487' },
-//   { title: 'Registration', content: 'Finished' },
-//   { title: 'Included games', content: '58' }
-// ];
-// const user = { rank: 6 };
-// const gameCarousel = {
-//   images: [
-//     { image: 'games/small/game-1.png', position: 1 },
-//   ],
-//   total: 16
-// };
+import GameCarousel from './GameCarousel';
+import GameDescription from './GameDescription';
+import RankingHeader from './RankingHeader';
+import RankingTable from './RankingTable';
+
 export default {
   name: 'base-modal',
   props: {
     game: { type: Object, required: true },
-    ranking: { type: Object, required: true },
+    ranking: { type: Array, required: true },
     userRank: { type: Number, default: null },
-    gameStateDescription: { type: Object, required: true },
+    gameStats: { type: Array, required: true },
     gameCarousel: { type: Object, required: true }
   },
   computed: { currentRouteName: vm => vm.$route.name },
